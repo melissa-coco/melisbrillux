2ebde828ed70dd43579d75852f3cb12b13759b09
Melissa Coco <melissa@MacBook-Air-di-Melissa.local>
2026-05-21 21:27:47 +0200

    fix: remove Oi from font select

--- Tipografia Cinetica/app.js
+++ Tipografia Cinetica/app.js
@@ -264,7 +264,7 @@ function buildControls() {
       <select id="font-select">
         <option value="Lato" ${state.font === 'Lato' ? 'selected' : ''}>Lato</option>
         <option value="Times New Roman" ${state.font === 'Times New Roman' ? 'selected' : ''}>Times New Roman</option>
-        <option value="Oi" ${state.font === 'Oi' ? 'selected' : ''}>Oi</option>
+
       </select>
     </div>
     <div class="control-group">
